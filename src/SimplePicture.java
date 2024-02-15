e0ec8ac6b4bdf08d0e29f6cd1d39e18cb6f3a4aa
dtrainor <djtrainor@icloud.com>
2024-02-15 09:12:55 -0800

    AP CS PixLab, IntelliJ

--- src/SimplePicture.java
+++ src/SimplePicture.java
@@ -361,6 +361,9 @@ public class SimplePicture implements DigitalPicture
         return pixelArray;
     }
 
+
+
+
     /**
      * Method to load the buffered image with the passed image
      * @param image  the image to use
